76a523772dcd16794cb4e0aceb118c53cc050d9b
prasanna-ezhilmurugan <prasanna.ezhilmurugan@gmail.com>
2025-03-27 15:45:20 +0530

    change user

--- app/lib/placeholder-data.js
+++ app/lib/placeholder-data.js
@@ -1,8 +1,8 @@
 const users = [
   {
     id: '410544b2-4001-4271-9855-fec4b6a6442a',
-    name: 'User',
-    email: 'user@nextmail.com',
+    name: 'Dinesh',
+    email: 'dinesh@gmail.com',
     password: '123456',
   },
 ];
